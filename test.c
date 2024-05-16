56da0d339e7d4662a50346814374a1158bb68a77
Marius Meier <mmeier@c1r3p5.hive.fi>
2024-05-16 16:44:28 +0300

    adds first draft of tokenizer function

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: mmeier <mmeier@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 16:24:39 by mmeier            #+#    #+#             */
-/*   Updated: 2024/05/15 16:21:28 by mmeier           ###   ########.fr       */
+/*   Updated: 2024/05/16 12:30:52 by mmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,15 @@ void	print_env(char **env)
 
 /*The 'readline' function enables writing commands to program during execution. 
   In case 'env' is typed to shell, env. variables are printed to the terminal, 
-  otherwise command that user inputs is printed. The 'add_history' function enables 
-  visibility of previous inserted commands (using keyboards "arrow up" after a command
-  has been typed)*/
+  otherwise command that user inputs is printed. The 'add_history' function 
+  enables visibility of previous inserted commands (using keyboards "arrow up" 
+  after a command has been typed)*/
 int	ft_input(char **env)
 {
 	char	*input;
-	char	*environment = "env";
+	char	*environment;
 
+	environment = "env";
 	while (1)
 	{
 		input = readline("minishell> ");
@@ -54,9 +55,9 @@ int	ft_input(char **env)
 	return (0);
 }
 
-/*String array 'env' holds by default environment variables of the system. Array
-  still needs to be copied before doing any alterations, but it's already possible
-  to print it. */
+/*String array 'env' holds by default environment variables of the system. 
+  Array still needs to be copied before doing any alterations, but it's 
+  already possible to print it. */
 int	main(int ac, char *av[], char *env[])
 {
 	if (ac > 1 || av[1])
