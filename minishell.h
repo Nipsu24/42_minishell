f17551e386a4aaa5df97ff3d79aa56ce39fb6463
Marius Meier <mmeier@c1r4p3.hive.fi>
2024-05-20 16:59:27 +0300

    prints entered string into seperate words, introduces several structs (not yet used and functional) and a function to assign command types to the printed words (not yet functional)

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: mmeier <mmeier@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 12:39:08 by mmeier            #+#    #+#             */
-/*   Updated: 2024/05/15 16:00:04 by mmeier           ###   ########.fr       */
+/*   Updated: 2024/05/20 16:32:21 by mmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,34 @@ typedef struct s_placeholder
 	int		placeholder;
 }				t_placeholder;
 
+typedef enum s_token_type
+{
+	COMMAND,
+	STRING,
+	REDIRECT_IN,
+	REDIRECT_OUT,
+	REDIRECT_IN_DEL,
+	REDIRECT_OUT_APP,
+	FILE_NAME,
+	PIPE,
+	ENVAR,
+}			t_token_type;
+
+typedef struct s_token
+{
+	char			*cnt;
+	t_token_type	type;
+}				t_token;
+
+typedef struct s_tokens
+{
+	int			index;
+	t_token		*entry;	
+}				t_tokens;
+
 int		ft_input(char **env);
 void	print_env(char **env);
 char	**ft_tokenize(char const *s, char c, char str_flag);
+void	ft_token_type(t_tokens *tokens, int i);
 
 #endif
