86cf52a7489105771a77c1fe1e723760da975378
Cesar Sanchez <cesasanc@c1r6p14.hive.fi>
2024-05-30 15:19:34 +0300

    Modified version of signals

--- signals.c
+++ signals.c
@@ -6,29 +6,45 @@
 /*   By: cesasanc <cesasanc@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 16:15:01 by cesasanc          #+#    #+#             */
-/*   Updated: 2024/05/29 18:23:27 by cesasanc         ###   ########.fr       */
+/*   Updated: 2024/05/30 15:18:56 by cesasanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	handle_this(pid_t sig, siginfo_t *info, void *context)
+static void	handle_this(int sig)
 {
-	(void)context;
-	(void)info;
 	(void)sig;
-	if (sig == SIGQUIT)
-		ft_putstr_fd("Quit: 3\n", 1);
+	ft_printf("\nminishell>");
 }
 
 void	setup_signal(void)
 {
-	struct sigaction	sa;
+	struct sigaction	sa_int;
+	struct sigaction	sa_quit;
 
-	sa.sa_sigaction = handle_this;
-	sa.sa_flags = SA_SIGINFO | SA_RESTART;
-//	if (sigemptyset(&sa.sa_mask) == -1);
-//		clean_exit_shell();
-	if (sigaction(SIGQUIT, &sa, NULL) == -1)
-		sa.sa_handler = SIG_IGN;
+	sa_int.sa_handler = handle_this;
+	sa_int.sa_flags = SA_RESTART;
+	if (sigemptyset(&sa_int.sa_mask) == -1)
+	{
+		perror("sigemptyset");
+		exit(EXIT_FAILURE);
+	}
+	if (sigaction(SIGINT, &sa_int, NULL) == -1)
+	{
+		perror("sigaction: SIGINT");
+		exit(EXIT_FAILURE);
+	}
+	sa_quit.sa_handler = SIG_IGN;
+	sa_quit.sa_flags = SA_RESTART;
+	if (sigemptyset(&sa_quit.sa_mask) == -1)
+	{
+		perror("sigemptyset");
+		exit(EXIT_FAILURE);
+	}
+	if (sigaction(SIGQUIT, &sa_quit, NULL) == -1)
+	{
+		perror("sigaction: SIGQUIT");
+		exit(EXIT_FAILURE);
+	}
 }
