2519fd1999bcd412cddf98b346d3d1868725205f
César <cesasanc@student.hive.fi>
2024-08-14 12:54:12 +0300

    Updated builtin utils

--- builtins/builtin_utils.c
+++ builtins/builtin_utils.c
@@ -6,17 +6,78 @@
 /*   By: cesasanc <cesasanc@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 14:21:17 by cesasanc          #+#    #+#             */
-/*   Updated: 2024/06/05 17:38:20 by cesasanc         ###   ########.fr       */
+/*   Updated: 2024/08/14 12:32:25 by cesasanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-bool	needs_arg(char **comand_array)
+int	find_var(char **env, char *word)
 {
-	if (comand_array[0][0] == )
+	int	i;
+	int	j;
 
-	if (!ft_strncmp("pwd", comand_array[0][0], 3)
-		|| !ft_strncmp("cd", comand_array[0][0], 2))
-		return(TRUE);
+	i = 0;
+	j = len_array(env);
+	if (!env)
+		return (0);
+	while ((i < j) && ft_strncmp(word, env[i], ft_strlen(word)))
+		i++;
+	return (i);
+}
+
+int	add_var(t_data *data, char *var)
+{
+	int		i;
+	int		j;
+	char	**new_env;
+
+	i = 0;
+	j = len_array(data->temp_env);
+	new_env = malloc(sizeof(char *) * (j + 2));
+	if (!new_env)
+		return (1);
+	while (i < j)
+	{
+		new_env[i]
+			= ft_substr(data->temp_env[i], 0, ft_strlen(data->temp_env[i]));
+		if (!new_env[i])
+			free_arr_rev(new_env, i);
+		i++;
+	}
+	new_env[i] = ft_substr(var, 0, ft_strlen(var));
+	if (!new_env[i])
+		free_arr_rev(new_env, i);
+	new_env[i + 1] = NULL;
+	free_arr(data->temp_env);
+	data->temp_env = new_env;
+	free_arr(new_env);
+	return (0);
+}
+
+int	update_var(t_data *data, char *var)
+{
+	int	i;
+
+	i = find_var(data->temp_env, var);
+	if (i == len_array(data->temp_env))
+		return (add_var(data, var));
+	free(data->temp_env[i]);
+	data->temp_env[i] = ft_substr(var, 0, ft_strlen(var));
+	if (!data->temp_env[i])
+		return (1);
+	free_str(&var);
+	return (0);
+}
+
+int	len_array(char **array)
+{
+	int	i;
+
+	i = 0;
+	if (!array)
+		return (0);
+	while (array[i])
+		i++;
+	return (i);
 }
