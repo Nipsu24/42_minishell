3f76f0d68bc3c5e68f908a9d9e3da896d38bec67
César <cesasanc@student.hive.fi>
2024-08-16 14:18:21 +0300

    Added unset.c

---
+++ builtins/unset.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cesasanc <cesasanc@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/15 23:53:35 by cesasanc          #+#    #+#             */
+/*   Updated: 2024/08/16 13:17:28 by cesasanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	unset(t_data *data)
+{
+	int		i;
+	char	**tmp;
+
+	tmp = ft_split(data->proc[data->j].cmd[1], '=');
+	i = find_var(data->temp_env, tmp[0]);
+	if (i == len_array(data->temp_env))
+		return (0);
+	while (i < len_array(data->temp_env))
+	{
+		data->temp_env[i] = data->temp_env[i + 1];
+		i++;
+	}
+	data->temp_env[i] = NULL;
+	return (0);
+}
