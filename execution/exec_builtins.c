f54c10c1134734b383ed2fe253ee9c4981f943f1
Marius Meier <mmeier@c1r5p17.hive.fi>
2024-08-30 17:03:26 +0300

    fixes conditional jump when conducting parent builins

--- execution/exec_builtins.c
+++ execution/exec_builtins.c
@@ -6,7 +6,7 @@
 /*   By: mmeier <mmeier@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 14:54:59 by mmeier            #+#    #+#             */
-/*   Updated: 2024/08/29 14:55:19 by mmeier           ###   ########.fr       */
+/*   Updated: 2024/08/30 16:56:39 by mmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,6 +57,26 @@ int	non_child_builtins(t_data *data)
 	{
 		unset(data);
 		return (1);
+	}
+	return (0);
+}
+
+int	parent_builtin_check(t_data *data)
+{
+	data->j = 0;
+	while (data->j < data->proc_nbr)
+	{
+		if (!data->proc[data->j].cmd)
+			return (0);
+		if (ft_strncmp(data->proc[data->j].cmd[0], "cd", 3) == 0)
+			return (1);
+		else if (ft_strncmp(data->proc[data->j].cmd[0], "exit", 5) == 0)
+			return (1);
+		else if (ft_strncmp(data->proc[data->j].cmd[0], "export", 7) == 0)
+			return (1);
+		else if (ft_strncmp(data->proc[data->j].cmd[0], "unset", 6) == 0)
+			return (1);
+		data->j++;
 	}
 	return (0);
 }
