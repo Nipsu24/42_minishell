d35018e5c3ad94e50417b41c61c6b86e1e4550e5
César <cesasanc@student.hive.fi>
2024-09-17 17:40:04 +0300

    Fixed pwd

--- builtins/pwd.c
+++ builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: cesasanc <cesasanc@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 17:11:07 by cesasanc          #+#    #+#             */
-/*   Updated: 2024/09/16 22:27:12 by cesasanc         ###   ########.fr       */
+/*   Updated: 2024/09/17 17:38:06 by cesasanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,11 @@
 int	pwd(t_data *data)
 {
 	char	cwd[BUFF_SIZE];
-	char	*home;
-
-	home = get_env_var(data, "HOME");
-	if (!home || !*home)
-		return (1);
+	
 	if (!getcwd(cwd, sizeof(cwd)))
 	{
 		update_exit_status(data, 1, "pwd", "Error getting cwd");
-		return (chdir(home), 1);
+		return (1);
 	}
 	printf("%s\n", cwd);
 	return (0);
